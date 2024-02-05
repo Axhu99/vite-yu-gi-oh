a1db0f02077b1ecd5aacadff7e6399b8f771b64e
Axhu99 <xhuliodreni@icloud.com>
2024-02-05 20:24:15 +0100

    add fontawesome and background-color

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
 import "@fontsource/montserrat";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { createApp } from 'vue'
+import App from './App.vue'
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 
 createApp(App).mount('#app')

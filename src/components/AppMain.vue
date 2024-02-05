8eb537c215628aa3cb8879b129ab7a3b7fa38426
Axhu99 <xhuliodreni@icloud.com>
2024-02-05 21:29:36 +0100

    get API

---
+++ src/components/AppMain.vue
@@ -0,0 +1,13 @@
+<script>
+import CharactersList from './characters/characterslist.vue';
+export default {
+    name: 'AppMain',
+    components: { CharactersList }
+}
+</script>
+
+<template>
+    <main class="container">
+        <CharactersList />
+    </main>
+</template>
---
+++ src/components/characters/CharactersList.vue
@@ -0,0 +1,21 @@
+<script>
+import axios from 'axios';
+const endpoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons'
+export default {
+    name: 'CharactersList',
+    data: () => ({
+        characters: [],
+    }),
+    created() {
+        axios.get(endpoint).then(res => {
+            this.characters = res.data.docs;
+        })
+    }
+
+};
+</script>
+
+<template>
+    <section id="characters">
+    </section>
+</template>
